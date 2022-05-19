1307c601e22d4ed7a281122b18cba5f73da5305c
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2022-05-19 09:04:04 -0500

    trying a script for universal last modified.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -33,7 +33,8 @@ document.addEventListener('DOMContentLoaded', function (event) {
         imageModal.style.display = "none";
     }
 
-    setIframeVideo()
+    setIframeVideo();
+    setModifiedDate();
 });
 
 function setIframeVideo() {
@@ -52,4 +53,19 @@ window.onresize = setIframeVideo;
 function getContentWidth(element) {
     let styles = getComputedStyle(element)
     return element.clientWidth - parseFloat(styles.paddingLeft) - parseFloat(styles.paddingRight)
+}
+
+function setModifiedDate() {
+    if (document.getElementById('last-modified')) {
+        fetch("https://api.github.com/repos/{{ site.github.owner_name }}/{{ site.github.repository_name }}/commits?path={{ page.path }}")
+            .then((response) => {
+                return response.json();
+            })
+            .then((commits) => {
+                var modified = commits[0]['commit']['committer']['date'].slice(0,10);
+                if(modified != "{{ page.date | date: "%Y-%m-%d" }}") {
+                    document.getElementById('last-modified').textContent = "Last Modified: " + modified;
+                }
+            });
+    }
 }
